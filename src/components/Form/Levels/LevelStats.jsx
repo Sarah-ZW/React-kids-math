592df1e39d73e78114e54bddc8a64885703e0e27
Sarah-ZW <earthclaninfo@gmail.com>
2024-06-28 23:08:37 -0400

    levels work

--- src/components/Form/Levels/LevelStats.jsx
+++ src/components/Form/Levels/LevelStats.jsx
@@ -17,8 +17,10 @@ export function LevelStats({
   advSubLevel,
 }) {
   const [showLevels, setShowLevels] = useState(false)
+  const [allLevelsComplete, setAllLevelsComplete] = useState(true)
+
   return (
-    <>
+    <div className="levels">
       <h2
         onClick={() => {
           setShowLevels((current) => !current)
@@ -27,37 +29,85 @@ export function LevelStats({
       >
         Levels
       </h2>
-      {showLevels && (
-        <>
-          <p className="flex-row">
-            10 correct in a row advances you one level for that category
-          </p>
-          <p className="flex-row">Complete level 10 to complete the category</p>
-          <div className="flex-row">
-            <div>
-              <h3>Beginner</h3>
-              <p>Beginner Addition: {begAddLevel}</p>
-              <p>Beginner Subtraction: {begSubLevel}</p>
-              <p>Beginner Multiplication: {begMulLevel}</p>
-              <p>Beginner Division: {begDivLevel}</p>
+      {showLevels &&
+        (!allLevelsComplete ? (
+          <>
+            <p className="flex-row">
+              10 correct in a row advances you one level for that category
+            </p>
+            <p className="flex-row">
+              Complete level 10 to complete the category
+            </p>
+            <div className="flex-row">
+              <div>
+                <h3>Beginner</h3>
+                <p>
+                  Beginner Addition:{" "}
+                  {begAddLevel >= 2 ? "Complete!" : begAddLevel}
+                </p>
+                <p>
+                  Beginner Subtraction:{" "}
+                  {begSubLevel >= 2 ? "Complete!" : begSubLevel}
+                </p>
+                <p>
+                  Beginner Multiplication:{" "}
+                  {begMulLevel >= 2 ? "Complete!" : begMulLevel}
+                </p>
+                <p>
+                  Beginner Division:{" "}
+                  {begDivLevel >= 2 ? "Complete!" : begDivLevel}
+                </p>
+              </div>
+              <div>
+                <h3>Intermediate</h3>
+                <p>
+                  Intermediate Addition:{" "}
+                  {medAddLevel >= 2 ? "Complete!" : medAddLevel}
+                </p>
+                <p>
+                  Intermediate Subtraction:{" "}
+                  {medSubLevel >= 2 ? "Complete!" : medSubLevel}
+                </p>
+                <p>
+                  Intermediate Multiplication:{" "}
+                  {medMulLevel >= 2 ? "Complete" : medMulLevel}
+                </p>
+                <p>
+                  Intermediate Division:{" "}
+                  {medDivLevel >= 2 ? "Complete" : medDivLevel}
+                </p>
+              </div>
+              <div>
+                <h3>Advanced</h3>
+                <p>
+                  Advanced Addition:{" "}
+                  {advAddLevel >= 2 ? "Complete" : advAddLevel}
+                </p>
+                <p>
+                  Advanced Subtraction:{" "}
+                  {advSubLevel >= 2 ? "Complete" : advSubLevel}
+                </p>
+                <p>
+                  Advanced Multiplication:{" "}
+                  {advMulLevel >= 2 ? "Complete" : advMulLevel}
+                </p>
+                <p>
+                  Advanced Division:{" "}
+                  {advDivLevel >= 2 ? "Complete" : advDivLevel}
+                </p>
+              </div>
             </div>
-            <div>
-              <h3>Intermediate</h3>
-              <p>Intermediate Addition: {medAddLevel}</p>
-              <p>Intermediate Subtraction: {medSubLevel}</p>
-              <p>Intermediate Multiplication: {medMulLevel}</p>
-              <p>Intermediate Division: {medDivLevel}</p>
-            </div>
-            <div>
-              <h3>Advanced</h3>
-              <p>Advanced Addition: {advAddLevel}</p>
-              <p>Advanced Subtraction: {advSubLevel}</p>
-              <p>Advanced Multiplication: {advMulLevel}</p>
-              <p>Advanced Division: {advDivLevel}</p>
-            </div>
+          </>
+        ) : (
+          <div>
+            {" "}
+            <p>
+              &quot; You have completed all the levels! You are a champion
+              rockstar with the most math power of anyone I&apos;ve seen in a
+              long long time!&quot;
+            </p>
           </div>
-        </>
-      )}
-    </>
+        ))}
+    </div>
   )
 }
